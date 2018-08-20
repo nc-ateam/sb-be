7b6f6fdeb3d12fe2b5f0b17c9f3d4796ddb99398
John Harrington <32112213+jxh01753@users.noreply.github.com>
2018-08-20 10:06:15 +0100

    Implements connection to db

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const { DB_URL } = require('./db/config');
 const cors = require('cors');
 const { rootRouter } = require('./routes/rootRouter');
 const { apiRouter } = require('./routes/apiRouter');
 
 app.use(cors());
+
+mongoose
+    .connect(
+        DB_URL,
+        { useNewUrlParser: true }
+    )
+    .then(() => {
+        console.log(`Connection to mongoose is live on ${DB_URL}`);
+    });
 
 app.use('/', rootRouter);
 app.use('/api', apiRouter);
