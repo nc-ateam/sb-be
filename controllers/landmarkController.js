1bbbf8300acbb7f694120a3fd976cdac756573a6
John Harrington <32112213+jxh01753@users.noreply.github.com>
2018-08-20 11:26:03 +0100

    Implements getLandmarksByCityID

--- controllers/landmarkController.js
+++ controllers/landmarkController.js
@@ -10,4 +10,14 @@ const getAllLandmarks = (req, res, next) => {
         .catch(next);
 };
 
-module.exports = { getAllLandmarks };
+const getLandmarksByCity = (req, res, next) => {
+    Landmark.find({ belongs_to: { _id: req.params.city_id } })
+        .populate('belongs_to')
+        .lean()
+        .then((landmarks) => {
+            res.status(200).send({ landmarks });
+        })
+        .catch(next);
+};
+
+module.exports = { getAllLandmarks, getLandmarksByCity };
