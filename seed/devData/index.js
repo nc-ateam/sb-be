1bf951326c023a952d9222bdce5fefcdbf03419e
jbromley94 <jordanbromley94@gmail.com>
2018-08-20 11:48:19 +0100

    contains devData for seeding, provides a larger array of landmarks and users

---
+++ seed/devData/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  citiesData: require("./cities"),
+  countriesData: require("./countries"),
+  landmarksData: require("./landmarks"),
+  photosData: require("./photos"),
+  usersData: require("./users")
+};
