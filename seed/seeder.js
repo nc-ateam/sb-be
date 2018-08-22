ae043930e534f9c5e1c6dd94806f0c076ab2966d
jbromley94 <jordanbromley94@gmail.com>
2018-08-22 10:41:13 +0100

    altercation to DBURL

--- seed/seeder.js
+++ seed/seeder.js
@@ -1,4 +1,4 @@
-const { DB_URL } = require('../db/config');
+const DB_URL = require('../db/config');
 const data = require('./testData');
 const seedDB = require('./seed');
 const mongoose = require('mongoose');
